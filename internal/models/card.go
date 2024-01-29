2541cf6b0fd818750c680e32ebb1d68fd051ea76
Graham Hargrove <hargrove.g.s@gmail.com>
2024-01-29 12:48:33 -0500

    Retrieve card value (#3)
    * Retrieve tcgplayer pricing for each kids cards
    
    * Store & retrieve the cards kind to be used when determining which price set to use
    
    * Require the card kind when creating a new card
    
    * Updated comment
    
    * Updated migration
    
    * Updated doc comment
    
    ---------
    
    Co-authored-by: Graham Hargrove <graham@walkofff.com>

--- internal/models/card.go
+++ internal/models/card.go
@@ -1,7 +1,47 @@
 package models
 
 type Card struct {
-	Id    int    `json:"id"`
-	TcgId string `db:"tcg_id" json:"tcg_id"`
-	Name  string `json:"name"`
+	Id     int    `json:"id"`
+	TcgId  string `db:"tcg_id" json:"tcg_id"`
+	Name   string `json:"name"`
+	Kind   string `json:"kind"`
+	Prices Value  `json:"prices"`
+}
+
+/** Find the prices for the card based on its kind (normal, holo, etc) */
+func (card *Card) LookupValues(prices TCGPlayerPrices) {
+	if card.Kind == "normal" && prices.Normal != nil {
+		card.Prices = *prices.Normal
+	}
+
+	if card.Kind == "holofoil" && prices.Holofoil != nil {
+		card.Prices = *prices.Holofoil
+	}
+
+	if card.Kind == "reverse-holofoil" && prices.ReverseHolofoil != nil {
+		card.Prices = *prices.ReverseHolofoil
+	}
+}
+
+type TCGPlayerPrices struct {
+	Holofoil *struct {
+		Low    float64 `json:"low"`
+		Mid    float64 `json:"mid"`
+		High   float64 `json:"high"`
+		Market float64 `json:"market"`
+	} `json:"holofoil,omitempty"`
+
+	ReverseHolofoil *struct {
+		Low    float64 `json:"low"`
+		Mid    float64 `json:"mid"`
+		High   float64 `json:"high"`
+		Market float64 `json:"market"`
+	} `json:"reverseHolofoil,omitempty"`
+
+	Normal *struct {
+		Low    float64 `json:"low"`
+		Mid    float64 `json:"mid"`
+		High   float64 `json:"high"`
+		Market float64 `json:"market"`
+	} `json:"normal,omitempty"`
 }
