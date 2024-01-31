62ecb1861410f2a785af7c4fde759543291eae77
Graham Hargrove <graham@walkofff.com>
2024-01-31 16:37:01 -0500

    Fixed tailwind imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -40,7 +40,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl text-red-500 font-bold underline">Hello world!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
