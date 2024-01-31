f3c5f80e0e52b8752e4e1cfb1d189895cb31bb1e
Graham Hargrove <graham@walkofff.com>
2024-01-31 15:20:14 -0500

    Installed tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,33 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
-    console.log('Hello world')
+    console.log("Hello world");
 
-    fetch('http://localhost:8082/api/kids', {
+    fetch("http://localhost:8082/api/kids", {
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      method: 'GET'
+      method: "GET",
     })
-    .then((res) => {
-      if (res.ok) {
-        return res.json()
-      }
-    }).then((data) => {
-      console.log(data)
-    }).catch((e) => {
-      console.log('THIS DIDNT WORK')
-      console.log(e)
-    })
-  }, [])
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((e) => {
+        console.log("THIS DIDNT WORK");
+        console.log(e);
+      });
+  }, []);
 
   return (
     <>
@@ -38,6 +40,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -50,7 +53,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
