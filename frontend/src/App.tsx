0dfc0b189fe368a838d604e02538d79d49edd532
Graham Hargrove <graham@walkofff.com>
2024-01-31 22:23:23 -0500

    Installed react-query and react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect} from "react";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+const client = new QueryClient()
 
+function App() {
   useEffect(() => {
     console.log("Hello world");
 
@@ -30,29 +29,9 @@ function App() {
   }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h1 className="text-3xl text-red-500 font-bold underline">Hello world!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <QueryClientProvider client={client}>
+      <h1>Hello world</h1>
+    </QueryClientProvider>
   );
 }
 
---
+++ frontend/src/routes/index.lazy.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { createLazyFileRoute } from '@tanstack/react-router'
+
+const Index: React.FC = () => {
+  return (
+    <div className="p-2">
+      <h3>Welcome Home!</h3>
+    </div>
+  )
+}
+
+export const Route = createLazyFileRoute('/')({
+  component: Index
+})
