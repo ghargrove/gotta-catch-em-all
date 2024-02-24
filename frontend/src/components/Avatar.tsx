113720e874639bf59ec271e4a0487a70f9e1cd39
Graham Hargrove <graham@walkofff.com>
2024-02-24 14:38:12 -0500

    Change users query from select to a query x to make sure all user results are returned

--- frontend/src/components/Avatar.tsx
+++ frontend/src/components/Avatar.tsx
@@ -3,6 +3,10 @@ import eevee from "~/assets/eevee.png";
 import charmander from "~/assets/charmander.png";
 import pokeball from "~/assets/pokeball.png";
 
+/**
+ * Presents an avater image based on the users profile. Avatar
+ * will default to a pokeball
+ */
 export const Avatar: React.FC<{ id: number }> = (props) => {
   const { id } = props;
 
